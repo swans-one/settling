a1cdf224b525c12d58b6138683eb94ecde693e5f
Erik Swanson <theerikswanson@gmail.com>
2014-02-09 14:47:36 -0500

    Add BoardType interface.
    This interface will allow for setup of different board layouts. It
    takes care of converting between ordinal and hexagonal coordinate
    systems.
    
    We start with a StandardBoardType concrete implementation of the
    BoardType interface.

---
+++ settling/board_types.py
@@ -0,0 +1,35 @@
+"""Board types are mappings between a hexagonal coordinate system and
+ordinal numbers.
+
+This mapping allows for different board sizes to be defined, and a
+simple list of tiles/numbers/ports to be passed in for the
+construction.
+
+Currently the only board type defined is the standard 3-4 player
+board.
+
+"""
+
+from abc import ABCMeta, abstractmethod
+
+class BoardType(metaclass=ABCMeta):
+    @abstractmethod
+    def hexagon_from_ordinal(self, ordinal):
+        """Return a class defined hexagon coordinate given an ordinal.
+        """
+        pass
+
+    @abstractmethod
+    def ordinal_from_hexagon(self, hexagon_coords):
+        """Return a class defined ordinal given a hexagon coordinate.
+        """
+        pass
+
+class StandardBoardType(BoardType):
+    """The standard 3-4 player catan board.
+    """
+    def hex_from_ordinal(self, ordinal):
+        pass
+
+    def ordinal_from_hex(self, hex_coords):
+        pass
