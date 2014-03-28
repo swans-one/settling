77a95310d1d542e91bcbf20d95dccb7c3e1ab5b1
Erik Swanson <theerikswanson@gmail.com>
2014-03-28 17:32:09 -0400

    Better module doc-string for hex_utils.

--- settling/hex_utils.py
+++ settling/hex_utils.py
@@ -1,4 +1,18 @@
 """Utilities for dealing with hex grids.
+
+This file establishes three hex grid coordinate systems.
+
+1) Ordinal -- A system of numbering a grid of hexagons starting at 0
+   and extending to +Inf.
+
+2) Hexagon -- A system in in N3, where coordinates are 3-tuples
+   representing coordinates in the three directions naturally
+   associated with the six sides of a hexagon.
+
+3) RSP (Ring Spline Offset -- A system in N3, where coordinates are
+   3-tuples of a ring coordinate, a spline coordinate, and an offset
+   coordinate.
+
 """
 
 def neighbors(hex_coord):
