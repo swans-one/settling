b5eaf06f04eb598517824022ea8b5d50ac7060a6
Erik Swanson <theerikswanson@gmail.com>
2014-03-29 20:34:41 -0400

    Test class outline

--- settling/test/test_hex_utils.py
+++ settling/test/test_hex_utils.py
@@ -6,7 +6,7 @@ class Test_neighbors(unittest.TestCase):
     def test_origin_neighbors(self):
         """The neighbors of the origin should be equal to the delta.
         """
-        deltas = [(1,0,-1), (0,1,-1), (-1,1,0), 
+        deltas = [(1,0,-1), (0,1,-1), (-1,1,0),
                   (-1,0,1), (0,-1,1), (1,-1,0)]
         neighbors = hex_utils.neighbors((0, 0, 0))
         self.assertEqual(neighbors, deltas)
@@ -18,6 +18,33 @@ class Test_neighbors(unittest.TestCase):
         self.assertIn((2,1,-3), neighbors)
         self.assertIn((1,2,-3), neighbors)
         self.assertIn((1,0,-1), neighbors)
-        
-        
 
+class Test_hexagon_from_ordinal(unittest.TestCase):
+    pass
+
+class Test_hexagon_from_rso(unittest.TestCase):
+    pass
+
+class Test_ordinal_from_hexagon(unittest.TestCase):
+    pass
+
+class Test_ordinal_from_rso(unittest.TestCase):
+    pass
+
+class Test_rso_from_hexagon(unittest.TestCase):
+    pass
+
+class Test_rso_from_ordinal(unittest.TestCase):
+    pass
+
+class Test__find_ring(unittest.TestCase):
+    pass
+
+class Test__find_spine(unittest.TestCase):
+    pass
+
+class Test__find_offset(unittest.TestCase):
+    pass
+
+class Test__tiles_in_ring(unittest.TestCase):
+    pass
