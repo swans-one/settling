ed7dd7569e75d005ea72854b2a3a555273e806f7
Erik Swanson <theerikswanson@gmail.com>
2014-03-29 20:02:45 -0400

    Copies over code from board_types.
    We'll use the code from board_types where applicable, and flesh out
    the rest of the methods as needed.
    
    I'll try to get the tests from test_board_types working before I move
    on to implementing new methods, though.

--- settling/hex_utils.py
+++ settling/hex_utils.py
@@ -68,7 +68,7 @@ show how they are calculated and related.
 """
 
 def neighbors(hex_coord):
-    deltas = [(1,0,-1), (0,1,-1), (-1,1,0), 
+    deltas = [(1,0,-1), (0,1,-1), (-1,1,0),
               (-1,0,1), (0,-1,1), (1,-1,0)]
     neighbors = []
     for delta in deltas:
@@ -79,7 +79,17 @@ def neighbors(hex_coord):
 def hexagon_from_ordinal(ordinal_coord):
     """Convert from an ordinal to a hexagon coordinate.
     """
-    pass
+    # Check the cache before computing
+    if ordinal in self.cached_hexagon_from_ordinal:
+        return self.cached_hexagon_from_ordinal[ordinal]
+
+    # If the value is not in the cache, compute it explicitly
+    ring, spine, offset = self.ring_spine_offset_from_ordinal(ordinal)
+    hexagon_coords = self.hexagon_from_ring_spine_offset(ring, spine, offset)
+
+    #cache computed value
+    self.cached_hexagon_from_ordinal[ordinal] = hexagon_coords
+    return hexagon_coords
 
 def hexagon_from_rso(rso_coord):
     """Convert from a ring spline offset to a hexagon coordinate.
@@ -89,7 +99,29 @@ def hexagon_from_rso(rso_coord):
 def ordinal_from_hexagon(hexagon_coord):
     """Convert from a hexagon to an ordianl coordinate.
     """
-    pass
+    # First check that the coordinates are valid
+    if sum(hex_coords) != 0:
+        raise ValueError("{0} are not valid hexagon coordinates".format(hex_coords))
+     # Go to the cache before iterating
+    if hex_coords in self.cached_ordinal_from_hexagon:
+        return self.cached_ordinal_from_hexagon[hex_coords]
+
+    # If the cache fails, iterate through all the ordinal numbers,
+    # comparing to their hexagon coordinates to those given.
+    #
+    # This method ensures that the ordering is consistent in both
+    # directions.
+    MAX_SEARCH = 1000
+    current_ordinal = 0
+    while current_ordinal < MAX_SEARCH:
+        hexagon = self.hexagon_from_ordinal(current_ordinal)
+        if hexagon == hex_coords:
+            # cache computed value
+            self.cached_ordinal_from_hexagon[hex_coords] = current_ordinal
+            return current_ordinal
+        current_ordinal += 1
+    err_msg = "Cannot find coordinates in first {0} ordinals"
+    raise ValueError(err_msg.format(MAX_SEARCH))
 
 def ordinal_from_rso(rso_coord):
     """Convert from a ring spline offset to an ordinal coordinate.
@@ -104,4 +136,38 @@ def rso_from_hexagon(hexagon_coord):
 def rso_from_ordinal(ordinal_coord):
     """Convert from an ordinal to a ring spline offset coordinate.
     """
-    pass
+    ring = self._find_ring(ordinal)
+    spine = self._find_spine(ordinal, ring)
+    offset = self._find_offset(ordinal, ring)
+    return ring, spine, offset
+
+
+def _find_ring(ordinal, ring_so_far=0):
+    """Recursively traverses triangular numbers.
+    """
+    check = ordinal - (6 * ring_so_far)
+    if check <= 0:
+        return ring_so_far
+    else:
+        return self._find_ring(check, ring_so_far + 1)
+
+def _find_spine(ordinal, ring):
+    if ordinal == 0:
+        return 0
+    tiles_before_ring = sum(self._tiles_in_ring(r) for r in range(ring))
+    ordinal_in_ring = ordinal - tiles_before_ring
+    tiles_per_spine = self._tiles_in_ring(ring) // 6
+    spine = ordinal_in_ring // tiles_per_spine
+    return spine
+
+def _find_offset(ordinal, ring):
+    if ordinal == 0:
+        return 0
+    tiles_before_ring = sum(self._tiles_in_ring(r) for r in range(ring))
+    ordinal_in_ring = ordinal - tiles_before_ring
+    tiles_per_spine = self._tiles_in_ring(ring) // 6
+    offset = ordinal_in_ring % tiles_per_spine
+    return offset
+
+def _tiles_in_ring(ring):
+    return 1 if ring == 0 else ring * 6
