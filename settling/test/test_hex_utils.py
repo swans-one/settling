6863fb40f274ade628a7603ef9b153490add6428
Erik Swanson <theerikswanson@gmail.com>
2014-03-31 22:22:11 -0400

    Added (failing) tests.

--- settling/test/test_hex_utils.py
+++ settling/test/test_hex_utils.py
@@ -20,13 +20,54 @@ class Test_neighbors(unittest.TestCase):
         self.assertIn((1,0,-1), neighbors)
 
 class Test_hexagon_from_ordinal(unittest.TestCase):
-    pass
+    def test_works(self):
+        """A few assertions to test that this works.
+
+        Since the underlying componenets of this are thoroughly
+        tested, this amounts to an integration test.
+        """
+        self.assertEqual(hex_utils.hexagon_from_ordinal(22), (0, 3, -3))
+        self.assertEqual(hex_utils.hexagon_from_ordinal(11), (-2, 2, 0))
+        self.assertEqual(hex_utils.hexagon_from_ordinal(32), (1, -3, 2))
+        self.assertEqual(hex_utils.hexagon_from_ordinal(36), (3, -1, -2))
+
+    def test_repeated_calculations(self):
+        """Check that cache returns the same values.
+
+        Since values, are cached, we want to make sure the lookup
+        returns the same value as the calculation.
+        """
+        self.assertEqual(hex_utils.hexagon_from_ordinal(36), (3, -1, -2))
+        self.assertEqual(hex_utils.hexagon_from_ordinal(36), (3, -1, -2))
 
 class Test_hexagon_from_rso(unittest.TestCase):
     pass
 
 class Test_ordinal_from_hexagon(unittest.TestCase):
-    pass
+    def test_works(self):
+        """A simple test that the correct ordinal is returned.
+        """
+        self.assertEqual(hex_utils.ordinal_from_hexagon((3, -1, -2)), 36)
+
+    def test_round_trip(self):
+        """Test that going through a round trip works.
+
+        We send a value first through hexagon_from_ordinal, then
+        through ordinal_from_hexagon, and compare to the original.
+        """
+        start_ordinal = 77
+        end_hexagon = hex_utils.hexagon_from_ordinal(start_ordinal)
+        end_ordinal = hex_utils.ordinal_from_hexagon(end_hexagon)
+        self.assertEqual(start_ordinal, end_ordinal)
+
+    def test_repeated_calculations(self):
+        """Check that cache returns the same values.
+
+        Since values, are cached, we want to make sure the lookup
+        returns the same value as the calculation.
+        """
+        self.assertEqual(hex_utils.ordinal_from_hexagon((3, -1, -2)), 36)
+        self.assertEqual(hex_utils.ordinal_from_hexagon((3, -1, -2)), 36)
 
 class Test_ordinal_from_rso(unittest.TestCase):
     pass
