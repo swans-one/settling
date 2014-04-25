f1cb2a0c8e82623e1a701a06e7b5685fcd3b08c0
Erik Swanson <theerikswanson@gmail.com>
2014-04-24 23:24:45 -0400

    Test for add_town.

--- settling/test/test_board.py
+++ settling/test/test_board.py
@@ -1,3 +1,4 @@
+from unittest.mock import MagicMock
 import unittest
 
 from board_geometry import StandardBoard
@@ -23,3 +24,16 @@ class Test_Board__set_up(unittest.TestCase):
     def test_creates_tiles(self):
         b = board.Board(self.tiles, self.numbers, self.ports, self.board_geom)
         self.assertTrue(b._tiles)
+
+
+class Test_Board_add_town(unittest.TestCase):
+    def setUp(self):
+        """Pin the method under test to a mock object.
+        """
+        self.board = MagicMock()
+        self.board._vertices = {}
+        self.board.add_town = board.Board.add_town
+
+    def test_town_added(self):
+        self.board.add_town(self.board, (0, 0, 0), 0, 'player1')
+        self.assertEqual(len(self.board._vertices), 1)
