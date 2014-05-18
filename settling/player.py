37a3342ab5fcaf15f074fdcafda9b0db7f625203
Erik Swanson <theerikswanson@gmail.com>
2014-05-18 13:55:02 -0400

    Player ABC.

--- settling/player.py
+++ settling/player.py
@@ -1,3 +1,20 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
+from abc import ABCmeta, abstractmethod
+
+
+class Player(metaclass=ABCmeta):
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @abstractmethod
+    def starting_town(self, board):
+        pass
+
+    @abstractmethod
+    def play_action_card(self, board, hand):
+        pass
+
+    @abstractmethod
+    def act(self, board, hand):
+        pass
