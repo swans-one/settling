fcfb5ad4a73b860b504a40b172b1d8a047501366
Erik Swanson <theerikswanson@gmail.com>
2014-05-18 23:29:14 -0400

    Player return value doc-strings.

--- settling/player.py
+++ settling/player.py
@@ -5,16 +5,34 @@ class Player(metaclass=ABCmeta):
     @property
     @abstractmethod
     def name(self):
+        """
+        Returns:
+          A string, unique between players. The real life player's name.
+        """
         pass
 
     @abstractmethod
     def starting_town(self, board):
+        """
+        Returns:
+          A tuple (hexagon_coord, vertex) containing the location to place
+          the town on.
+        """
         pass
 
     @abstractmethod
     def play_action_card(self, board, hand):
+        """
+        Returns:
+          An instance of a player_action.PlayerActionCard or None.
+        """
         pass
 
     @abstractmethod
     def act(self, board, hand):
+        """
+        Returns:
+          An instance of a class from settling.player_action. If no action
+          is to be taken, return EndTurn.
+        """
         pass
